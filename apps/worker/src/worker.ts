1af8584a039392cd0e5d4d840cefb81d025a419e
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-25 11:46:07 -0300

    fix: use named ioredis constructor in worker

--- apps/worker/src/worker.ts
+++ apps/worker/src/worker.ts
@@ -1,5 +1,5 @@
 import { Worker } from "bullmq";
-import Redis from "ioredis";
+import { Redis } from "ioredis";
 import { MarketProductionService } from "@nova-aurora/database";
 
 function connectionOptions(): {
